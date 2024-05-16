c110ef0041bfc038f611b591a8b7781e582d3d80
Colson <colsoncalifornication@gmail.com>
2024-05-16 13:54:30 +0545

    render props pattern: searchable list

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,74 @@
  * Example: Accordion (Accordion & AccordionItem)
  */
 
+/* @ Render Props Pattern */
+/*
+ * The core idea behind this Render Props pattern is that, we pass a function
+ * as a value for the children prop or actually for any prop but it's most
+ * commonly done for the children prop.
+ * So the idea is that we have one component that defines some function,
+ * which important, must return something renderable. So a function in a
+ * component where that function in the component then returns something
+ * renderable and it's then this function which is passed as a value for
+ * the children prop.
+ * So between those component tags to another component i.e component two, and
+ * that component then returns the result of calling that received function
+ * which is why this function must return something renderable.
+ *
+ * Example:
+ * Reusable SearchableList Component, which should be a component
+ * that displays a search input field and contains the logic for searching
+ * through the items that are passed to that list.
+ * And the idea is that this component can be used on different kinds of data,
+ * and very important, that this component is all about containing that search
+ * logic, but not really about controlling how the items should be rendered.
+ * So we wanna build a component that contains a lot of logic, possibly
+ * also some complex logic, but that's not really involved too much with
+ * rendering the items that are searchable.
+ * And that's a usecase where render props can help us!
+ */
+
 import Accordion from './components/Accordion/Accordion';
+import SearchableList from './components/SearchableList/SearchableList';
+
+import savannaImg from './assets/african-savanna.jpg';
+import amazonImg from './assets/amazon-river.jpg';
+import caribbeanImg from './assets/caribbean-beach.jpg';
+import desertImg from './assets/desert-dunes.jpg';
+import forestImg from './assets/forest-waterfall.jpg';
+
+const PLACES = [
+  {
+    id: 'african-savanna',
+    image: savannaImg,
+    title: 'African Savanna',
+    description: 'Experience the beauty of nature.',
+  },
+  {
+    id: 'amazon-river',
+    image: amazonImg,
+    title: 'Amazon River',
+    description: 'Get to know the largest river in the world.',
+  },
+  {
+    id: 'caribbean-beach',
+    image: caribbeanImg,
+    title: 'Caribbean Beach',
+    description: 'Enjoy the sun and the beach.',
+  },
+  {
+    id: 'desert-dunes',
+    image: desertImg,
+    title: 'Desert Dunes',
+    description: 'Discover the desert life.',
+  },
+  {
+    id: 'forest-waterfall',
+    image: forestImg,
+    title: 'Forest Waterfall',
+    description: 'Listen to the sound of the water.',
+  },
+];
 
 function App() {
   return (
@@ -64,6 +131,9 @@ function App() {
             </Accordion.Content>
           </Accordion.Item>
         </Accordion>
+
+        <SearchableList items={PLACES} />
+        <SearchableList items={['city 1', 'city 2', 'city 3']} />
       </section>
     </main>
   );
